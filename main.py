da2701e6fb356b8e23138a38de01d05a2b7749a6
Spatialflunky1 <mjandrus06@gmail.com>
2021-11-27 17:50:45 +0000

    fixed up main.py

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import RPi.GPIO as GPIO
 
+# This is the GPIO channel that I use 
 Relay_channel = [4]
 
+# Sets up the GPIO ports for use
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(Relay_channel, GPIO.OUT, initial=GPIO.LOW)
-# GPIO.output(4, GPIO.HIGH)
+
+# You can use these two functions in the application
+def on():
+    GPIO.output(4, GPIO.LOW)
+
+def off():
+    GPIO.output(4, GPIO.HIGH)
