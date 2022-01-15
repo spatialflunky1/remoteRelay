7b2b7e77dea21ecfb811b21ac40eecba08e32cac
Spatialflunky1 <mjandrus06@gmail.com>
2022-01-15 12:32:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import RPi.GPIO as GPIO
+import sys
 
 # This is the GPIO channel that I use 
 Relay_channel = [4]
 
-# Sets up the GPIO ports for use
+# Sets up the GPIO config
+GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(Relay_channel, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(Relay_channel, GPIO.OUT, initial=GPIO.HIGH)
 
 # You can use these two functions in the application
 def on():
@@ -13,3 +15,12 @@ def on():
 
 def off():
     GPIO.output(4, GPIO.HIGH)
+
+if len(sys.argv) != 2:
+  print("Invalid arguments!")
+  quit()
+
+if sys.argv[1] == "on":
+  on()
+elif sys.argv[1] == "off":
+  off()
